b09527fa4973a81b8f8691f77f4fd2e4256c3bc6
BAYRAM <bayramismail69@gmail.com>
2021-05-01 23:47:44 +0300

    4. day end

--- interfaces/src/interfaces/CustomerManager.java
+++ interfaces/src/interfaces/CustomerManager.java
@@ -2,23 +2,22 @@ package interfaces;
 
 public class CustomerManager {
 	private Logger[] loggers;
+
 	public CustomerManager(Logger[] logger) {
 		super();
 		this.loggers = logger;
 	}
 
-	//loosly - tightly coupled
+	// loosly - tightly coupled
 	public void add(Customer customer) {
 		System.out.println("Customer added " + customer.getFirstName());
-		//this.logger.log(customer.getFirstName());
-		for (Logger logger : loggers) {
-			logger.log(customer.getFirstName());
-		}
+		// this.logger.log(customer.getFirstName());
+		Utils.runLoggers(loggers, customer.getFirstName());
 	}
 
 	public void delete(Customer customer) {
 		System.out.println("Customer deleted");
-		
+		Utils.runLoggers(loggers, customer.getFirstName());
 	}
 
 }
